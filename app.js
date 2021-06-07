1ee2291e22c007de6cfd7b29e7163944be64f9b6
AlexSelMarburg <Selalx2@aol.com>
2021-06-07 17:43:37 +0200

    added hero picking

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 "use strict";
 
 let inFight = false;
+const heroDataArray = JSON.parse(localStorage.getItem("savegame")) || [];
+
+// initiate
+document.addEventListener('DOMContentLoaded', function (e) {
+
+    const heroCards = document.querySelectorAll('#heroes-container .hero-card')
+    const heroDataCards = document.querySelectorAll('.hero-data-card')
+
+    // heroCards.querySelector('.hero-portrait').style.backgroundImage = heroDataArray[0].portrait;
+    console.log(heroDataCards)
+
+    for (let i = 0; i < heroCards.length; i++) {
+        heroCards[i].querySelector('.hero-portrait').style.backgroundImage = heroDataArray[i].portrait;
+    }
+
+    for (let i = 0; i < heroDataCards.length; i++) {
+        heroDataCards[i].querySelector('.hero-name').textContent = heroDataArray[i].name;
+    }
+
+});
 
 //--toggle-fullscreen----------------------------------------------------
 const btnToggleFullScreen = document.querySelector("#toggle-screen-size-btn")
@@ -33,30 +53,56 @@ const btnInventory = document.querySelector('#toggle-inventory-btn');
 
 btnInventory.onclick = function (evt) {
     const heroCards = document.querySelectorAll('.hero-card');
-    const timer = ms => new Promise(res => setTimeout(res, ms))
+    const timer = ms => new Promise(res => setTimeout(res, ms));
     const partyInventory = document.querySelector('#party-inventory');
+    const partyStatsCards = document.querySelectorAll('.hero-data-card');
 
     btnInventory.disabled = true;
 
     async function flip() {
+
+        toggleInventory();
+        toggleHeroStats();
+
         for (let i = 0; i < heroCards.length; i++) {
             heroCards[i].querySelector(".hero-card-inner").classList.toggle("flip-card");
-            await timer(210);
+            await timer(150); //time passed after each cardflip (next iteration)
         }
-        await timer(400);
+
+        await timer(800); // time bevor btn is anabled again
         btnInventory.disabled = false;
     }
 
     async function toggleInventory() {
 
         if (!inFight) {
-            await timer(1300);
-            partyInventory.classList.toggle("fadeOut");
-            partyInventory.classList.toggle("fadeIn");
+            if (partyInventory.classList.contains("fadeOut")) {
+                await timer(1200);
+                partyInventory.classList.toggle("fadeOut");
+                partyInventory.classList.toggle("fadeIn");
+            } else {
+                partyInventory.classList.toggle("fadeOut");
+                partyInventory.classList.toggle("fadeIn");
+            }
         }
 
     }
 
+    async function toggleHeroStats() {
+
+        if (!inFight) {
+            await timer(1200);
+            for (let statsCard of partyStatsCards) {
+                if (statsCard.classList.contains("fadeOut")) {
+                    statsCard.classList.toggle("fadeOut");
+                    statsCard.classList.toggle("fadeIn");
+                } else {
+                    statsCard.classList.toggle("fadeOut");
+                    statsCard.classList.toggle("fadeIn");
+                }
+            }
+        }
+    }
+
     flip();
-    toggleInventory();
 }
